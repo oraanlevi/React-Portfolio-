826b83d72210d63fe4b76dff0e888aeb18c76d6d
Oraan Levi <oraanlevi1@gmail.com>
2023-01-24 18:09:47 -0800

    Updates

---
+++ precache-manifest.eda8a287aa58dd54b366f097d16ae9b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ada19cab6c87249826a436d7ed652f",
+    "url": "/React-Portfolio-/index.html"
+  },
+  {
+    "revision": "e9d7f8838f2c975537d3",
+    "url": "/React-Portfolio-/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "217a5a4b36515faa378f",
+    "url": "/React-Portfolio-/static/css/main.f81682a0.chunk.css"
+  },
+  {
+    "revision": "e9d7f8838f2c975537d3",
+    "url": "/React-Portfolio-/static/js/2.516fa331.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-Portfolio-/static/js/2.516fa331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217a5a4b36515faa378f",
+    "url": "/React-Portfolio-/static/js/main.b755f0d4.chunk.js"
+  },
+  {
+    "revision": "eaec9a65851cf62c4d5d",
+    "url": "/React-Portfolio-/static/js/runtime-main.c84e93e6.js"
+  }
+]);
